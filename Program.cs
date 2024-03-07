f5e6d2aa35929ef8a6d683093a756186239ffff2
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-07 14:43:52 +0200

    update -> first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args){
+            string name = "Ratchanon Traitiprat\n";
+            Console.WriteLine("Jamk University of Applied Science");
+            Console.WriteLine("Hello, "+name);
+            Console.WriteLine("Number : "+Console.ReadLine());
+            Console.WriteLine("{2} - {1} = {0}",5,10,15);
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
