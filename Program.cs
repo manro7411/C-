535d264aed1f57ee0e58912e7680c9b4ae2a5d49
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-07 15:07:27 +0200

    update -> const name

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace Name
     {
         static void Main(string[] args){
             string name = "Ratchanon Traitiprat\n";
+            string hello = "Hello world";
             Console.WriteLine("Jamk University of Applied Science");
             Console.WriteLine("Hello, "+name);
             Console.WriteLine("{2} - {1} = {0}",5,10,15);
@@ -12,15 +13,21 @@ namespace Name
             {
                 Console.WriteLine(i);
             }
-            // Declare variable 
             int x = 10;int y = 20;
+            x = 100;
             int result = x + y;
             Console.WriteLine("Result : "+result);
-
             float a =10.2f;
             float b = 10f;
             Console.WriteLine("The first number : "+a+"\nThe second number :"+b);
+            double number1 = 10.2;
+            double number2 = 11.5;
+            Console.WriteLine("Result Double :"+number1+number2);
+            Console.WriteLine(string.Compare(name,hello));
 
+            const float PI = 3.14f; // Constant variable 
+            Console.ReadKey();
+            
         }
     }
 }
