61af65bed38a9658629f3fa4fb324715b21bfd06
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-07 14:53:59 +0200

    update -> Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 namespace Name
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args){
             string name = "Ratchanon Traitiprat\n";
             Console.WriteLine("Jamk University of Applied Science");
             Console.WriteLine("Hello, "+name);
-            Console.WriteLine("Number : "+Console.ReadLine());
             Console.WriteLine("{2} - {1} = {0}",5,10,15);
-            
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(i);
             }
+            // Declare variable 
+            int x = 10;int y = 20;
+            int result = x + y;
+            Console.WriteLine("Result : "+result);
+
+            float a =10.2f;
+            float b = 10f;
+            Console.WriteLine("The first number : "+a+"\nThe second number :"+b);
 
         }
     }
